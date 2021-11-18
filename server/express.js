ffbdd2367cc3f1ad901727852f415cee9afc7f60
christopher augusto <christopher@cromaqui.com>
2021-11-18 19:29:44 -0300

    enable async server init and stop

--- server/express.js
+++ server/express.js
@@ -1,28 +1,46 @@
+const http = require('http');
+const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const compress = require('compression');
 const cors = require('cors');
 const helmet = require('helmet');
 const userRoutes = require('./routes/user');
+const config = require('../config');
+let app;
 
-const CURRENT_WORKING_DIR = process.cwd();
-const app = require('express')();
+let httpServer;
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieParser());
-app.use(compress());
-app.use(helmet());
-app.use(cors());
+const initializeWebServer = () => {
+  return new Promise((resolve, reject) => {
+    app = express();
 
-app.use('/', userRoutes);
-app.use((err, req, res, next) => {
-  if (err.name === 'UnauthorizedError') {
-    res.status(401).json({"error": `${err.name}: ${err.message}`});
-  } else if (err) {
-    res.status(400).json({"error": `${err.name}: ${err.message}`});
-    console.log(err);
-  }
-});
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(cookieParser());
+    app.use(compress());
+    app.use(helmet());
+    app.use(cors());
 
-module.exports = app;
+    app.use('/', userRoutes);
+
+    httpServer = http.createServer(app);
+
+    httpServer.listen(3000, (err) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+
+      resolve(app);
+    });
+  });
+};
+
+const stopWebServer = () => {
+  return new Promise((resolve, reject) => {
+    httpServer.close(() => { resolve(); });
+  });
+};
+
+module.exports = { initializeWebServer, stopWebServer };
--- server/index.js
+++ server/index.js
@@ -1,11 +1,16 @@
 const config = require('../config');
-const db = require('./db');
-const app = require('./express');
+const db = require('./db/config');
+const { initializeWebServer } = require('./express');
+const axios = require('axios');
 
-db.setUp();
+async function start() {
+  await db.setUp();
+  await initializeWebServer();
+}
 
-app.listen(config.port, (err) => {
-  if (err) console.log(err);
-
-  console.info(`Server started on port ${config.port}`);
+start().then(() => {
+  console.log('The app has started successfully.');
 })
+.catch((error) => {
+  console.log('App occured during startup:', error);
+});;
